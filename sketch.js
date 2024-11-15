87c74e580f1ba1cde192e0c7969e6ee16663c0c7
MoumitaRoy14 <76441152+MoumitaRoy14@users.noreply.github.com>
2024-11-15 15:50:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+
+function preload(){
+  
+}
+
+function setup(){
+  createCanvas(600,200);
+  
+  // creating trex
+ 
+  
+  //adding scale and position to trex
+
+}
+
+
+function draw(){
+  //set background color 
+  background("white");
+  
+  //logging the y position of the trex
+
+  
+  //jump when space key is pressed
+
+  
+ 
+  
+  //stop trex from falling down
+  
+}
